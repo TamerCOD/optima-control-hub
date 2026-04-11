4178c1c341be91dbd38f387c37ab9210955326eb
koshechka <81679462+TamerCOD@users.noreply.github.com>
2026-04-11 18:49:48 +0600

    123

--- src/modules/sync-dep/utils/formatters.ts
+++
@@ -1,8 +0,0 @@
-export const getInitials = (name: string) => {
-  return name.split(' ').map(n => n[0]).join('').toUpperCase().substring(0, 2);
-};
-
-export const truncateText = (text: string, length: number = 50) => {
-  if (text.length <= length) return text;
-  return text.substring(0, length) + '...';
-};
